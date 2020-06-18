ae6ede81a6b2e3c26ba6894175ae93f166e58335
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-06-18 16:44:51 -0400

    add toast to ToastButton

--- src/components/common/ToastButton/ToastButton.js
+++ src/components/common/ToastButton/ToastButton.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 
-const ToastButton = () => {
+import { withToast } from "_helpers";
+
+const ToastButton = ({ addToast }) => {
+  const showToast = () => {
+    addToast("Your information has been sent!", { appearance: "success" });
+  };
+
   return (
-    <Button variant="contained" color="primary">
+    <Button variant="contained" color="primary" onClick={showToast}>
       Show Toast
     </Button>
   );
 };
 
-export default ToastButton;
+export default withToast(ToastButton);
